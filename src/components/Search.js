a5f8702c64bb5298bfe639a48b22145752e9f606
Seycha Oudeinguene <36196523+Oudeinguene@users.noreply.github.com>
2022-09-28 15:00:40 +0200

    up

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,29 +1,54 @@
 import { type } from "@testing-library/user-event/dist/type";
-import React from "react";
-import { Row, Col, Container } from "reactstrap";
+import React, {useState} from "react";
+import {Row, Col, Container, Table} from "reactstrap";
 import "./Search.css"
+import {Restaurant} from "./restaurant";
 
 function Search(){
-    return (
-        <>
-       <Row style={{ display: 'flex', alignItems: 'center', justifyContent: 'center'}}>                
-            <Col xs="3">
-            <div className="searchBar">
-            <input 
-                type="text" 
-                name="searchBar" 
-                id="searchBar" 
-                placeholder="Trouver votre restaurant" />
-            </div>
-            </Col>
-            <Col xs="2">
-            <button>
-                Valider
-            </button>
-            </Col>
-        </Row>
-        </>
-    )
+    ///////////////////////SEARCH ON A DATATABLE
+   const [query, setQuery] = useState("");
+   const keys = ["first_name", "last_name", "email"];
+   const search = (data) => {
+     return data.filter((item) =>
+       keys.some((key) => item[key].toLowerCase().includes(query))
+     );
+   };
+ return (
+   <div className="app">
+       <input
+         className="search"
+         placeholder="Trouvez votre Restaurant"
+         onChange={(e) => setQuery(e.target.value.toLowerCase())}
+       />
+     {<Table data={Search(Restaurant)} />}
+   </div>
+ );
+
 }
 
+///////////////////// API SEARCH
+
+/*function App() {
+    const [query, setQuery] = useState("");
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await axios.get(`http://localhost:5000?q=${query}`);
+            setData(res.data);
+        };
+        if (query.length === 0 || query.length > 2) fetchData();
+    }, [query]);
+
+    return (
+        <div className="app">
+            <input
+                className="search"
+                placeholder="Search..."
+                onChange={(e) => setQuery(e.target.value.toLowerCase())}
+            />
+            {<Table data={data} />}
+        </div>
+    );
+}*/
 export default Search;
