ad492a5fe2337f9734aff3ef00d75f0e11ba95c9
reinema <77593341+reinem@users.noreply.github.com>
2022-09-28 23:15:52 +0200

    Revert "up"
    This reverts commit 73173647ce68637039ca18216180f526db48272a, reversing
    changes made to 319218cbcb07679b000560df899ba993e6b7f6ba.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 
-import Main from "./pages/Main";
+import Main from "./components/Main";
 import Book from "./components/Book";
 import ThankYou from "./components/ThankYou";
 import NavBar from "./components/NavBar";
 import Search from "./components/Search";
 import { Routes, Route, Link } from "react-router-dom";
-import Account from "./pages/Account";
-import Connexion from "./pages/Connexion";
+import Account from "./components/Account";
+import Connexion from "./components/Connexion";
 export default (_) => {
   const [page, setPage] = useState(0);
 
--- front-end/src/components/Main.js
+++ front-end/src/components/Main.js
@@ -1,20 +1,23 @@
 import React, { useState } from "react";
 import { Row, Col, Button } from "reactstrap";
-import Search from "../components/Search";
+import Search from "./Search";
 
-export default (props) => {
+
+export default props => {
   return (
-    <div className="main">
+    <div>
       <Row noGutters className="text-center align">
         <Col>
-          <Button color="blue" className="restaurantname-btn">
+          <Button
+            color="blue"
+            className="restaurantname-btn">
             Choisissez le restaurant
           </Button>
-
+          
           <Button
             color="none"
             className="book-table-btn"
-            onClick={(_) => {
+            onClick={_ => {
               props.setPage(1);
             }}
           >
@@ -23,17 +26,18 @@ export default (props) => {
         </Col>
       </Row>
       <Row>
-        <Search />
+      <Search />
       </Row>
       <Row noGutters className="text-center big-img-container">
         <Col>
           <img
-            src={require("../assets/img/restaurant.jpg")}
+            src={require("../images/restaurant.jpg")}
             alt="restaurant"
             className="big-img"
           />
         </Col>
       </Row>
+    
     </div>
   );
 };
