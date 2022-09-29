2206bdd46e642ac960a1ca97d6574d6647c1b558
Mux199 <73139732+Mux199@users.noreply.github.com>
2022-09-29 15:07:38 +0200

    up

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,11 @@
 import React, { useState } from "react";
 
 import Main from "./pages/Main";
-import Book from "./components/Book";
-import ThankYou from "./components/ThankYou";
 import NavBar from "./components/NavBar";
-import Search from "./components/Search";
 import { Routes, Route, Link } from "react-router-dom";
 import Account from "./pages/Account";
 import Connexion from "./pages/Connexion";
+import UserProfil from "./pages/UserProfil";
 export default (_) => {
   const [page, setPage] = useState(0);
 
@@ -18,6 +16,7 @@ export default (_) => {
         <Route path="/" element={<Main />} />
         <Route path="account" element={<Account />} />
         <Route path="connexion" element={<Connexion />} />
+        <Route path="userProfil" element={<UserProfil />} />
         <Route path="*" element={<Main />} />
       </Routes>
     </div>
---
+++ front-end/src/pages/UserProfil.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { AiOutlineUser } from "react-icons/ai";
+export default function UserProfil() {
+    return(
+        <div  className="userProfil"> 
+          <div>
+              Mon profil
+           </div> 
+            <div>
+           <  AiOutlineUser /> 
+            </div>
+        </div >
+
+       
+    )
+
+}
