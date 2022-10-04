0972414618163c5c9b892f0e7f3789174c559227
reinema <77593341+reinem@users.noreply.github.com>
2022-10-04 16:13:28 +0200

    up

---
+++ back-end/routes/restaurant.routes.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+const restaurantController = require("../controllers/restaurant.controller");
+
+//restaurant
+// router.get("/", restaurantController.getAllRestautants);
+// router.get("/:id", restaurantController.restaurantInfo);
+// router.put("/:id", restaurantController.updateRestaurant);
