b539635a278ce08144366fae03ea6206b8fd27cd
Mux199 <73139732+Mux199@users.noreply.github.com>
2022-10-04 22:21:32 +0200

    Profil du restaurant

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,6 +8,7 @@ import Connexion from "./pages/Connexion";
 import UserProfil from "./pages/UserProfil";
 import Logout from "./pages/Logout";
 import Faq from "./pages/Faq";
+import RestaurateurProfil from "./pages/RestaurateurProfil";
 export default (_) => {
   const [page, setPage] = useState(0);
 
@@ -21,6 +22,7 @@ export default (_) => {
         <Route path="userProfil" element={<UserProfil />} />
         <Route path="logout" element={<Logout />} />
         <Route path="faq" element={<Faq />} />
+        <Route path="restaurateurProfil" element={<RestaurateurProfil />} />
         <Route path="*" element={<Main />} />
       </Routes>
     </div>
