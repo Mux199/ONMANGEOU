05d982183c9464112fe5e784d22429d7e3a1d0f5
reinema <77593341+reinem@users.noreply.github.com>
2022-10-04 18:51:51 +0200

    UP

--- back-end/controllers/user.controller.js
+++ back-end/controllers/user.controller.js
@@ -8,35 +8,22 @@ module.exports.getAllUsers = async (req, res) => {
 module.exports.userInfo = (req, res) => {
   console.log(req.params);
   UserModel.findById(req.params.id, (err, docs) => {
-    if (!err) res.send(docs);
+    if (!err) res.status(200);
     else return res.status(400).send("id unknown : " + req.params.id);
   }).select("-password");
 };
 
 module.exports.updateUser = async (req, res) => {
-  await UserModel.findById(req.params.id, (err) => {
-    if (!err) {
-      try {
-        UserModel.findOneAndUpdate(
-          { _id: req.params.id },
-          {
-            $set: {
-              blocked: res.body.blocked,
-            },
-          },
-          { new: true, upsert: true, setDefaultsOnInsert: true },
-          (err, docs) => {
-            if (!err) return res.send(docs);
-            if (err) return res.status(500).send({ message: err + "pb" });
-          }
-        );
-      } catch (err) {
-        return res.status(500).send({ message: err });
-      }
-    } else return res.status(400).send("id unknown : " + req.params.id);
-  })
-    .clone()
-    .catch(function (err) {
-      console.log(err);
-    });
+  UserModel.updateOne(
+    { _id: req.params.id },
+    {
+      $set: {
+        telephone: res.body.telephone,
+      },
+    },
+    (err, docs) => {
+      if (!err) return res.status(docs);
+      if (err) return res.status(500).send({ message: err + "pb" });
+    }
+  );
 };
