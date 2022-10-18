c69b6c0ea34669cf2fb91f5e19f9909a5e7415a5
Mux199 <73139732+Mux199@users.noreply.github.com>
2022-10-18 14:29:56 +0200

    Account + modif Navbar

--- front-end/src/pages/Account.js
+++ front-end/src/pages/Account.js
@@ -8,8 +8,10 @@ export default function Account() {
   const [active, setActive] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
   const [nom, setNom] = useState("");
   const [prenom, setPrenom] = useState("");
+  const [telephone, setTelephone] = useState("");
 
 const handleLogin= (e) => {
   e.preventDefault();
@@ -17,8 +19,8 @@ const handleLogin= (e) => {
   const passwordError = document.querySelector(".password.error");
   const nomError = document.querySelector(".nom.error");
   const prenomError = document.querySelector(".prenom.error");
-
-
+  const telephoneError = document.querySelector(".telephone.error");
+  const passewordConfirm = document.querySelector(".passewordConfirm.error");
   axios( {
     method: "post",
     url:`${process.env.REACT_APP_API_URL}api/user/login`,
@@ -28,6 +30,8 @@ const handleLogin= (e) => {
       password,
       nom,
       prenom,
+      telephone
+
     }
 
   })
@@ -36,8 +40,9 @@ const handleLogin= (e) => {
     if (res.data.errors) {
       emailError.innerHTML = res.data.errors.email;
       passwordError.innerHTML = res.data.errors.password;
-      nomError.innerHTML = res.data.erros.nom;
-      prenomError.innerHTML = res.data.erros.prenom;
+      nomError.innerHTML = res.data.errors.nom;
+      prenomError.innerHTML = res.data.errors.prenom;
+      telephoneError.innerHTML= res.data.errors.telephone;
 
     } else {
       window.location = "/Connexion";
@@ -55,8 +60,8 @@ const handleLogin= (e) => {
             Nom
           </Label>
           <Input
-            id="Nom"
-            name="Nom"
+            id="nom"
+            name="nom"
             placeholder="Insérez votre nom"
             type="text"
             onChange={(e) => setNom(e.target.value)}
@@ -67,8 +72,8 @@ const handleLogin= (e) => {
         <FormGroup>
           <Label htmlFor="prenom">Prénom</Label>
           <Input
-            id="Prénom"
-            name="Prénom"
+            id="prenom"
+            name="prenom"
             placeholder="Insérez votre prénom"
             type="text"
             onChange={(e) => setPrenom(e.target.value)}
@@ -77,19 +82,22 @@ const handleLogin= (e) => {
         </FormGroup>
         <div className="prenom error"></div>
         <FormGroup>
-          <Label>Téléphone</Label>
+          <Label htmlFor="telephone">Téléphone</Label>
           <Input
-            id="Télephone"
-            name="Télephone"
-            placeholder="Num Télephone"
+            id="telephone"
+            name="telephone"
+            placeholder="Indiquez votre téléphone"
             type="number"
+            onChange={(e) => setTelephone(e.target.value)}
+            value={telephone}
           />
         </FormGroup>
+        <div className="telephone error"></div>
         <FormGroup>
           <Label htmlFor="email">Email</Label>
           <Input
-            id="Email"
-            name="Email"
+            id="email"
+            name="email"
             placeholder="Insérez votre mail"
             type="email"
             onChange={(e) => setEmail(e.target.value)}
@@ -98,10 +106,10 @@ const handleLogin= (e) => {
         </FormGroup>
         <div className="email error"></div>
         <FormGroup>
-          <Label htmlFor="Mot de passe">Mot de Passe</Label>
+          <Label htmlFor="passeword">Mot de Passe</Label>
           <Input
-            id="Password"
-            name="Mot de passe"
+            id="passeword"
+            name="passeword"
             placeholder="Insérez votre mot de passe"
             type="password"
             onChange={(e) => setPassword(e.target.value)}
@@ -109,6 +117,18 @@ const handleLogin= (e) => {
           />
         </FormGroup>
         <div className="password error"></div>
+        <FormGroup>
+          <Label htmlFor="passewordConfirm"> Confirmation du mot de passe</Label>
+          <Input
+            id="passewordConfirm"
+            name="passewordConfirm"
+            placeholder="Veuillez confirmer le mot de passe"
+            type="passeword"
+            onChange={(e) => setPasswordConfirm(e.target.value)}
+            value={passwordConfirm}
+         />
+        </FormGroup>
+        <div className="passwordConfirm error"></div>
         <FormGroup>
           <Label>Êtes vous un restaurateur?</Label>
           {!active && (
@@ -159,11 +179,11 @@ const handleLogin= (e) => {
           </>
         )}
         <div>
-          <Link to={'/Connexion'} >
+          {/* <Link to={'/Connexion'} > */}
           <Button className="valid-btn">
             Valider votre inscription
           </Button>
-          </Link>
+          {/* </Link> */}
         </div>
       </Form>
     </div>
