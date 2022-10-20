8f3cdb6cdaaaf338deb39852a1197d1e07ad7794
reinema <77593341+reinem@users.noreply.github.com>
2022-10-20 13:43:11 +0200

    up

--- front-end/src/pages/SignUpPro.js
+++ front-end/src/pages/SignUpPro.js
@@ -1,75 +1,71 @@
-import {React, useEffect,useRef,useState} from "react";
+import { React, useEffect, useRef, useState } from "react";
 import { AiOutlineUser } from "react-icons/ai";
 import { ProSidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
 import { Form, FormGroup, Label, Input, Button } from "reactstrap";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import axios from "axios";
 const styles = {
-    justifyContent: 'space-between'
-}
+  justifyContent: "space-between",
+};
 
-export default function SignUpPro(){
-    const [active, setActive] = useState(false);
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [passwordConfirm, setPasswordConfirm] = useState("");
-    const [nom, setNom] = useState("");
-    const [prenom, setPrenom] = useState("");
-    const [telephone, setTelephone] = useState("");
-    
-    const handleRegister= (e) => {
-        e.preventDefault();
-        const emailError = document.querySelector(".email.error");
-        const passwordError = document.querySelector(".password.error");
-        const nomError = document.querySelector(".nom.error");
-        const prenomError = document.querySelector(".prenom.error");
-        const telephoneError = document.querySelector(".telephone.error");
-        const passewordConfirm = document.querySelector(".passewordConfirm.error");
-        axios( {
-          method: "post",
-          url:`${process.env.REACT_APP_API_URL}api/user/register`,
-          withCredentials: true,
-          data: {
-            email,
-            password,
-            nom,
-            prenom,
-            telephone
-      
-          }
-        })
-        .then((res) => {
-          console.log(res);
-          if (res.data.errors) {
-            emailError.innerHTML = res.data.errors.email;
-            passwordError.innerHTML = res.data.errors.password;
-            nomError.innerHTML = res.data.errors.nom;
-            prenomError.innerHTML = res.data.errors.prenom;
-            telephoneError.innerHTML= res.data.errors.telephone;
-      
-          } else {
-            window.location = "/Connexion";
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-      };
-    return (
-     <div className="restau-profil" style={styles}> 
-        <Form onSubmit={handleRegister}>
-            <FormGroup>
-            <Label htmlFor="nom">
-                Nom
-            </Label>
-            <Input
-                id="nom"
-                name="nom"
-                placeholder="Insérez votre nom"
-                type="text"
-                onChange={(e) => setNom(e.target.value)}
-                value={nom}
-            />
+export default function SignUpPro() {
+  const [active, setActive] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [nom, setNom] = useState("");
+  const [prenom, setPrenom] = useState("");
+  const [telephone, setTelephone] = useState("");
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const emailError = document.querySelector(".email.error");
+    const passwordError = document.querySelector(".password.error");
+    const nomError = document.querySelector(".nom.error");
+    const prenomError = document.querySelector(".prenom.error");
+    const telephoneError = document.querySelector(".telephone.error");
+    const passwordConfirm = document.querySelector(".passwordConfirm.error");
+    axios({
+      method: "post",
+      url: `${process.env.REACT_APP_API_URL}api/user/register`,
+      withCredentials: true,
+      data: {
+        email,
+        password,
+        nom,
+        prenom,
+        telephone,
+      },
+    })
+      .then((res) => {
+        console.log(res);
+        if (res.data.errors) {
+          emailError.innerHTML = res.data.errors.email;
+          passwordError.innerHTML = res.data.errors.password;
+          nomError.innerHTML = res.data.errors.nom;
+          prenomError.innerHTML = res.data.errors.prenom;
+          telephoneError.innerHTML = res.data.errors.telephone;
+        } else {
+          window.location = "/ProProfil";
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <div className="restau-profil" style={styles}>
+      <Form onSubmit={handleRegister}>
+        <FormGroup>
+          <Label htmlFor="nom">Nom</Label>
+          <Input
+            id="nom"
+            name="nom"
+            placeholder="Insérez votre nom"
+            type="text"
+            onChange={(e) => setNom(e.target.value)}
+            value={nom}
+          />
         </FormGroup>
         <div className="nom error"></div>
         <FormGroup>
@@ -109,10 +105,10 @@ export default function SignUpPro(){
         </FormGroup>
         <div className="email error"></div>
         <FormGroup>
-          <Label htmlFor="passeword">Mot de Passe</Label>
+          <Label htmlFor="password">Mot de Passe</Label>
           <Input
-            id="passeword"
-            name="passeword"
+            id="password"
+            name="password"
             placeholder="Insérez votre mot de passe"
             type="password"
             onChange={(e) => setPassword(e.target.value)}
@@ -121,67 +117,63 @@ export default function SignUpPro(){
         </FormGroup>
         <div className="password error"></div>
         <FormGroup>
-          <Label htmlFor="passewordConfirm"> Confirmation du mot de passe</Label>
+          <Label htmlFor="passwordConfirm"> Confirmation du mot de passe</Label>
           <Input
-            id="passewordConfirm"
-            name="passewordConfirm"
+            id="passwordConfirm"
+            name="passwordConfirm"
             placeholder="Veuillez confirmer le mot de passe"
             type="password"
             onChange={(e) => setPasswordConfirm(e.target.value)}
             value={passwordConfirm}
-         />
+          />
         </FormGroup>
         <div className="passwordConfirm error"></div>
         <FormGroup>
-            <Label>Spécialité</Label>
-            <Input
-                id="specialite"
-                name="Spécialité"
-                placeholder="Veuillez insérer votre spécialité"
-                type="text"
-              />
+          <Label>Spécialité</Label>
+          <Input
+            id="specialite"
+            name="Spécialité"
+            placeholder="Veuillez insérer votre spécialité"
+            type="text"
+          />
         </FormGroup>
         <FormGroup>
-            <Label>Ville</Label>
-            <Input
-                id="city"
-                name="Ville"
-                placeholder="Veuillez insérer la ville dans laquelle vous vous situez"
-                type="text"
-            />
+          <Label>Ville</Label>
+          <Input
+            id="city"
+            name="Ville"
+            placeholder="Veuillez insérer la ville dans laquelle vous vous situez"
+            type="text"
+          />
         </FormGroup>
         <FormGroup>
-            <Label>Adresse du restaurant</Label>
-            <Input
-                id="Adresse"
-                name="Adresse"
-                placeholder="Veuillez insérer l'adresse du restaurant"
-                type="text"
-              />
+          <Label>Adresse du restaurant</Label>
+          <Input
+            id="Adresse"
+            name="Adresse"
+            placeholder="Veuillez insérer l'adresse du restaurant"
+            type="text"
+          />
         </FormGroup>
         <FormGroup>
-              <Label>Numéro de Siret</Label>
-              <Input
-                id="SIRET"
-                name="SIRET"
-                placeholder="Indiquez le numéro de SIRET"
-                type="number"
-              />
+          <Label>Numéro de Siret</Label>
+          <Input
+            id="SIRET"
+            name="SIRET"
+            placeholder="Indiquez le numéro de SIRET"
+            type="number"
+          />
         </FormGroup>
         <FormGroup>
-            <Label>Fourchette de prix</Label>
-            <Input 
-            />
+          <Label>Fourchette de prix</Label>
+          <Input />
         </FormGroup>
         <div>
-         <Link to={"/ProProfil"}>
-            <Button className="valid-btn" type="submit">
+          <Button className="valid-btn" type="submit">
             Valider votre inscription
-            </Button>
-        </Link> 
+          </Button>
         </div>
       </Form>
     </div>
-        
-    )
+  );
 }
