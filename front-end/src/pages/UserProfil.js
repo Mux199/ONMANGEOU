654a4960fbd5a0d4a9bc941676bd8593d0096700
reinem <77593341+reinem@users.noreply.github.com>
2022-10-21 10:22:40 +0200

    Update UserProfil.js

--- front-end/src/pages/UserProfil.js
+++ front-end/src/pages/UserProfil.js
@@ -1,25 +1,30 @@
-import {React} from "react";
-import SideBar from "../components/SideBar"
+import React, { useContext } from "react";
+import SideBar from "../components/SideBar";
 import { DatePicker } from "reactstrap-date-picker";
 import { useState } from "react";
 import { Label } from "reactstrap";
+import { UidContext } from "../AppContext";
+import { Navigate } from "react-router-dom";
+
+/* <DatePicker 
+                selected={startDate} 
+                onChange={(e) => setStartDate(date)} />*/
+
 const styles = {
-    display: 'flex',
-    justifyContent: 'space-between'
-}
+  display: "flex",
+  justifyContent: "space-between",
+};
 
- export default function UserProfil(){
-    const ChooseDate = () => {
-        const [startDate, setStartDate] = useState(new Date());
-    }
-    return (
-        <div className="user-profil" style={styles}>
-             <SideBar/> 
-             
-           
-            
- 
-        </div>
-    )
+export default function UserProfil() {
+  const uid = useContext(UidContext);
+  console.log(uid);
+  const ChooseDate = () => {
+    const [startDate, setStartDate] = useState(new Date());
+  };
+  return (
+    <div className="user-profil" style={styles}>
+      {/* {uid == null ? <Navigate to="/connexion" /> : <SideBar />} */}
+      <SideBar />
+    </div>
+  );
 }
-
