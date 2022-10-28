8167bae82f4a476c0c7ae299e9fd66fb7755dc34
Mux199 <73139732+Mux199@users.noreply.github.com>
2022-10-28 11:54:30 +0200

    Classe SignUp Pro

--- front-end/src/components/Search.js
+++ front-end/src/components/Search.js
@@ -1,6 +1,5 @@
-import { type } from "@testing-library/user-event/dist/type";
 import React from "react";
-import { Row, Col, Container,Button } from "reactstrap";
+import { Row, Col } from "reactstrap";
 import { Link } from "react-router-dom";
 
 function Search() {
