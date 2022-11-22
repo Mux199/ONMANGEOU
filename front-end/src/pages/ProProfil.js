72ff5ac2ee9ef81c21caa4894f4f88ed2dea436f
Seycha Oudeinguene <36196523+Oudeinguene@users.noreply.github.com>
2022-11-22 13:20:47 +0100

    up

--- front-end/src/pages/ProProfil.js
+++ front-end/src/pages/ProProfil.js
@@ -1,24 +1,53 @@
-import {React} from "react";
+import {React, useEffect} from "react";
 import SideBar from "../components/SideBar"
 import { DatePicker } from "reactstrap-date-picker";
 import { useState } from "react";
 import { Label } from "reactstrap";
-const styles = {
-    display: 'flex',
-    justifyContent: 'space-between'
-}
+import Restaurant from "../assets/restaurants.json";
+import Table_Reservation from "../components/Table_Reservation";
 
  export default function ProProfil(){
     const ChooseDate = () => {
         const [startDate, setStartDate] = useState(new Date());
     }
+
     const [query, setQuery] = useState("Tous");
+    const [filterData, setFilterData] = useState(Restaurant);
+
+     useEffect(() => {
+         let result = [...Restaurant];
+         //filtre barre de recherche (id,nom)
+         if (query) {
+             result = result.filter((item) =>  item.name.toLowerCase().includes(query.toLowerCase()));
+         }
+
+         setFilterData(result);
+
+     }, [query])
+
     return (
-        <div className="pro-profil" style={styles}>
-             <SideBar/>                
- 
-        </div>
-    )
-}
-
-
+        <div className="app">
+            <input
+                className="search"
+                placeholder="Trouvez la Réservation"
+                onChange={(e) => {
+                    setQuery(e.target.value.toLowerCase())
+                }}
+            />
+            <select
+                onChange={(e) => {
+                    setQuerySpeciality(e.target.value)
+                }}
+            >
+                <option value={"Tous"}>
+                    Date
+                </option>
+                {uniqueDate.map(dates => (
+                    <option value={specialitys}>
+                        {specialitys}
+                    </option>
+                ))}
+            {<Table_Reservation data={filterData}/>}
+            </div>
+        )
+        }
