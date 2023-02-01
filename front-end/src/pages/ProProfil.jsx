7482b146f2c32d7557a6c5a7361383b3b3c97f08
Seycha Oudeinguene <36196523+Oudeinguene@users.noreply.github.com>
2023-02-01 20:46:03 +0100

    Update ProProfil.jsx

--- front-end/src/pages/ProProfil.jsx
+++ front-end/src/pages/ProProfil.jsx
@@ -2,12 +2,12 @@ import {React, useEffect} from "react";
 import { useState } from "react";
 import TABLE_RESERVATION from "../components/TABLE_RESERVATION";
 import Reservation from "../assets/reservation.json";
-import DatePicker from 'react-datepicker';
+/*import DatePicker from 'react-datepicker';*/
 import 'react-datepicker/dist/react-datepicker.css';
 
  export default function ProProfil(){
-     const [startDate, setStartDate] = useState(new Date());
-     const [endDate, setEndDate] = useState(null);
+   /*  const [startDate, setStartDate] = useState(new Date());
+     const [endDate, setEndDate] = useState(null);*/
 
     const [query, setQuery] = useState("Tous");
     const [filterData, setFilterData] = useState(Reservation);
@@ -19,12 +19,12 @@ import 'react-datepicker/dist/react-datepicker.css';
              result = result.filter((item) =>  item.name.toLowerCase().includes(query.toLowerCase()) || item.id.toLowerCase().includes(query.toLowerCase()) || item.phone.toLowerCase().includes(query.toLowerCase()) || item.email.toLowerCase().includes(query.toLowerCase()) );
          }
          //filtre
-         if (startDate && endDate) {
+         /*if (startDate && endDate) {
                      result = result.filter((item) => startDate <= item.date && item.date <= endDate);
-         }
+         }*/
          setFilterData(result);
 
-     }, [query,startDate, endDate,])
+     }, [query/*,startDate, endDate,*/])
 
     return (
         <div className="app">
@@ -35,7 +35,7 @@ import 'react-datepicker/dist/react-datepicker.css';
                     setQuery(e.target.value.toLowerCase())
                 }}
             />
-            <DatePicker
+            {/*<DatePicker
                 selected={startDate}
                 onChange={date => {
                     setStartDate(date);
@@ -55,7 +55,7 @@ import 'react-datepicker/dist/react-datepicker.css';
                 endDate={endDate}
                 minDate={startDate}
                 dateFormat="dd/MM/yyyy"
-            />
+            /> */}
             <TABLE_RESERVATION data={filterData}/>
             </div>
 
