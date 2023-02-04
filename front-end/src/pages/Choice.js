e40dad7d882710950d5233df88b62ddfb6e3de03
Mux199 <73139732+Mux199@users.noreply.github.com>
2023-02-04 09:18:36 +0100

    CSS

--- front-end/src/pages/Choice.js
+++ front-end/src/pages/Choice.js
@@ -9,12 +9,12 @@ export default function Choice() {
         <div className="choice">
             <Menu iconShape="square" >
                 <MenuItem icon={<AiOutlineUser/>}>
-                    <Link to={'/signUpUser'} style={{ color: 'black', fontWeight:'bold'}}>
+                    <Link id={"Client"}to={'/signUpUser'} style={{ color: 'black', fontWeight:'bold'}}>
                         Vous êtes client
                     </Link>
                     </MenuItem>
                 <MenuItem icon= {<SiCodechef/>}>
-                    <Link to={'/signUpPro'} style={{ color: 'black', fontWeight:'bold'}} >
+                    <Link id={"Restaurateur"}to={'/signUpPro'} style={{ color: 'black', fontWeight:'bold'}} >
                         Vous êtes restaurateur
                     </Link>
                 </MenuItem>
