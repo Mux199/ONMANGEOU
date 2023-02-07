da6c64bc2fa57450e7289669ad194415151fcf48
reinema <77593341+reinem@users.noreply.github.com>
2023-02-07 14:48:03 +0100

    up

--- front-end/src/store/reducers/actions/addResa.actions.js
+++ front-end/src/store/reducers/actions/addResa.actions.js
@@ -2,12 +2,15 @@ import axios from "axios";
 
 export const ADD_RESA = "ADD_RESA";
 
-export const addResa = (data) => {
+export const addResa = (restaurant, user, nbClients, date, hour, lastname) => {
   return (dispatch) => {
     return axios
       .post(`${process.env.REACT_APP_API_URL}api/reservation/addReservation/`)
       .then((res) => {
-        dispatch({ type: ADD_RESA, payload: data });
+        dispatch({
+          type: ADD_RESA,
+          payload: { restaurant, user, nbClients, date, hour, lastname },
+        });
         console.log("res");
         console.log(res);
       })
